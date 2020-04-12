d3317b4c882a4d769f35574a947c72a036d1a230
Debajit Deb <debajitdeb11@gmail.com>
2020-04-12 14:40:18 +0530

    Initial Commit - Level 2 Athuntication

---
+++ app.js
@@ -0,0 +1,93 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb+srv://admin-debajit:dynamic11@test-loztw.mongodb.net/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+app.route('/')
+    .get(  (req, res) =>{
+        res.render('home')
+    } );
+
+app.route('/login')
+        .get(  (req, res) =>{
+            res.render('login')
+        })
+        .post(   (req, res) => {
+
+            const username = req.body.username;
+            const password = req.body.password;
+
+            User.findOne ({ 
+                email: username
+            },
+             (err, foundUser) => {
+                if(err){
+                    console.log(err)
+                    
+                }   else    {
+                    if(foundUser.password === password){
+                        res.render("secrets")
+                    }   else {
+                        res.render('login')
+                    }
+                }
+            })
+        }  
+    )
+
+ 
+
+app.route('/register')
+        .get(  (req, res) =>{
+            res.render('register')
+        })
+        .post(  (req, res) => {
+            const newUser = User(
+                {
+                    email: req.body.username,
+                    password: req.body.password,
+                }
+            )
+
+            newUser.save(   (err)=> {
+                if(err){
+                    console.log(err);
+                }   else {
+                    res.render('secrets')
+                }
+            } )
+        }
+        );
+
+app.route('/submit')
+        .get(  (req, res) =>{
+            res.render('submit')
+        });
+
+
+app.listen(3000, () => console.log("Server started at port 3000"));
