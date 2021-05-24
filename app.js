f9898d8f5ac34f7923edb2cc58832ac2f0d57196
Debajit Deb <debajitdeb11@gmail.com>
2021-05-24 12:18:52 +0530

    callback url modified

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "https://secret-share-app.herokuapp.com/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
